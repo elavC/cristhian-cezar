2301471e533da26bafd2ad1e17aa2ff374409977
CristhianCezar <cristhiancezardev@gmail.com>
2023-05-03 18:48:48 -0300

    mudando background para background-image

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-/* Scroll reveal - 
-Mostrar o elemento quando der scroll na página */
-
-const scrollReveal = ScrollReveal({
-    origin: "top",
-    distance: "5px",
-    duration: 700,
-    reset: true,
-  });
-  
-  scrollReveal.reveal(
-    `
-  .s-hero .title, 
-  .s-success .tag ,
-  .title-success,
-  .cards,
-  .card,
-  .s-about,
-  .about-me .my-picture,
-  .s-cases,
-  .title-cases,
-  .cases`,
-    { interval: 50 }
-  );
-  
   const nav = document.querySelector("header .container nav");
   
   const toggle = document.querySelectorAll(".toggle");
